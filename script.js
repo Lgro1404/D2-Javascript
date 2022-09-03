387193d52333ea6c8cff2084fd59c15cb70c1e7a
Luis <lgro1404@gmail.com>
2022-09-03 18:32:14 -0300

    quase-pronto

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
+var data
+var questao = -1;
+var respondido = false;
+var pontos = 0;
 function mostrarQuestao() {
-    document.getElementById("confirmar").classList.add("hide");
-    document.getElementById("listaRespostas").classList.remove("hide");
-}
-function loadClass(){
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.open("GET", "https://quiz-trainee.herokuapp.com/questions", true);
-    httpRequest.send(null);
-    httpRequest.onload = check(httpRequest);
-}
-function check(x){
-    if (x.readyState === 4){
-        alert("js succs");
+    for(i = 0; i < document.getElementsByName("resposta").length; i++){
+        if(document.getElementsByName("resposta")[i].checked){
+            respondido = true;
+            pontos += data[questao].options[i].value;
+            console.log(pontos);
+        }
     }
-    else{
-        alert(x.readyState);
+    if(respondido || questao < 0){
+        var xObj = new XMLHttpRequest();
+        xObj.open('GET', 'https://quiz-trainee.herokuapp.com/questions', true);
+        xObj.onload = function(){
+            data = JSON.parse(xObj.responseText);
+            if(questao >= data.length){
+                finalizarQuiz();
+                return;
+            }
+            document.getElementById("titulo").innerHTML = data[questao].title;
+            for(i = 1; i <= document.getElementsByName("resposta").length; i++){
+                document.getElementById("q" + i).innerHTML = data[questao].options[i-1].answer;
+            }
+        }
+        xObj.send();
+        for(i = 0; i < document.getElementsByName("resposta").length; i++){
+            document.getElementsByName("resposta")[i].checked = false;
+            respondido = false;
+        }
+        questao++;
+        document.getElementById("confirmar").innerHTML = "Próximo";
+        document.getElementById("listaRespostas").classList.remove("hide");
     }
 }
+
 function finalizarQuiz() {
-    
+    alert(pontos);
 }
